b3c550dfa8515611cc76d0da7bed38ca9f4b2dbb
Nandita Venkat <nanditavenkat@gmail.com>
2020-04-12 21:08:26 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def join_nested_strings(array)
   while count < array.length do
     inner=0
     while inner < array[count].length do
-      if array[count][inner].string?
+      if array[count][inner].is_a? String
         str = array[count][inner]
       end
       inner +=1
